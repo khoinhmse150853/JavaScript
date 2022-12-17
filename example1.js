efa59a31cd51133e6547d9144ee5483dcec288fd
khoinhmse150853 <khoinhmse150853@fpt.edu.vn>
2022-12-17 11:42:17 +0700

    variables and constant

--- example1.js
+++ example1.js
@@ -1,4 +1,6 @@
 function cyears(form) {
   console.log(form);
   form.dogage.value = form.myage.value * 7;
+  const constant = "constant";
+  document.getElementById("myConst").innerText = constant;
 }
---
+++ example2.js
@@ -0,0 +1,7 @@
+function simpleMath() {
+  var x = 10;
+  var y = 15;
+  var z = x + y;
+
+  document.getElementById("math").innerText = ++z;
+}
